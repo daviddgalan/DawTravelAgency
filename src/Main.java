83daff447650b71f83810d460538f351184b74ba
David Galan <davidgalan001@gmail.com>
2024-11-13 12:11:04 +0100

    Inserción de parte añadida por David Rosa en parte creada por Jesus Agapito

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -13,55 +14,49 @@ public class Main {
             switch (menu){
                 case 1:
                     do {
-                        System.out.print("Hola");
+                        System.out.println("Introduce el numero  de viajeros(máximo 5)");
+                        int viajeros= Integer.parseInt(s.nextLine());
+
+                        if (viajeros<0 || viajeros>5){
+                            System.out.println("El numero de viajeros debe de estar comprendido entre 0 y 5 ");
+                            do{
+                                System.out.println("Introduce el numero maximo de viajeros(máximo 5)");
+                                viajeros= Integer.parseInt(s.nextLine());
+                            }while(viajeros<0 || viajeros>5);
+                        }while(viajeros>=0 && viajeros<=5) {
+                            if(viajeros==1){
+                                System.out.println("Cuanto a pagado el viajero 1");
+                                int viajero1= Integer.parseInt(s.nextLine());
+                            }else {
+                                if(viajeros==2){
+                                    System.out.println("Cuanto a pagado el viajero 1");
+                                    int viajero1= Integer.parseInt(s.nextLine());
+                                    System.out.println("Cuanto a pagado el viajero 2");
+                                    int viajero2= Integer.parseInt(s.nextLine());
+                                }else {
+                                    if(viajeros==3){
+                                        System.out.println("Cuanto a pagado el viajero 1");
+                                        int viajero1= Integer.parseInt(s.nextLine());
+                                        System.out.println("Cuanto a pagado el viajero 2");
+                                        int viajero2= Integer.parseInt(s.nextLine());
+                                        System.out.println("Cuanto a pagado el viajero 3");
+                                        int viajero3= Integer.parseInt(s.nextLine());
+                                    }
+                                }
+                            }
+                        }
                     }while (menu < 2);
                     break;
 
                 case 2:
-                    do {
 
-
-                    }while ();
                     break;
                 case 3:
-                    do {
 
-                    }while ();
 
             }
         }while(menu < 4);
 
-        System.out.println("Introduce el numero  de viajeros(máximo 5)");
-        int viajeros= Integer.parseInt(s.nextLine());
 
-        System.out.print("Has salido");
-        if (viajeros<0 || viajeros>5){
-            System.out.println("El numero de viajeros debe de estar comprendido entre 0 y 5 ");
-            do{
-                System.out.println("Introduce el numero maximo de viajeros(máximo 5)");
-                 viajeros= Integer.parseInt(s.nextLine());
-            }while(viajeros<0 || viajeros>5);
-        }while(viajeros>=0&& viajeros<=5) {
-            if(viajeros==1){
-                System.out.println("Cuanto a pagado el viajero 1");
-                int viajero1= Integer.parseInt(s.nextLine());
-            }else {
-                if(viajeros==2){
-                    System.out.println("Cuanto a pagado el viajero 1");
-                    int viajero1= Integer.parseInt(s.nextLine());
-                    System.out.println("Cuanto a pagado el viajero 2");
-                    int viajero2= Integer.parseInt(s.nextLine());
-                }else {
-                    if(viajeros==3){
-                        System.out.println("Cuanto a pagado el viajero 1");
-                        int viajero1= Integer.parseInt(s.nextLine());
-                        System.out.println("Cuanto a pagado el viajero 2");
-                        int viajero2= Integer.parseInt(s.nextLine());
-                        System.out.println("Cuanto a pagado el viajero 3");
-                        int viajero3= Integer.parseInt(s.nextLine());
-                    }
-                }
-            }
-        }
     }
 }
