eb5ebdc3e56c3b35c2e64d2688ad6b3bfce5d398
davidrn06 <davidrosanebrera@gmail.com>
2024-11-13 11:13:23 +0100

    commit1 (introducir viajeros y cuanto pagan )

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+    Scanner s= new Scanner(System.in);
+        System.out.println("Introduce el numero  de viajeros(máximo 5)");
+        int viajeros= Integer.parseInt(s.nextLine());
 
-        System.out.println("dentro?");
+        if (viajeros<0 || viajeros>5){
+            System.out.println("El numero de viajeros debe de estar comprendido entre 0 y 5 ");
+            do{
+                System.out.println("Introduce el numero maximo de viajeros(máximo 5)");
+                 viajeros= Integer.parseInt(s.nextLine());
+            }while(viajeros<0 || viajeros>5);
+        }while(viajeros>=0&& viajeros<=5) {
+            if(viajeros==1){
+                System.out.println("Cuanto a pagado el viajero 1");
+                int viajero1= Integer.parseInt(s.nextLine());
+            }else {
+                if(viajeros==2){
+                    System.out.println("Cuanto a pagado el viajero 1");
+                    int viajero1= Integer.parseInt(s.nextLine());
+                    System.out.println("Cuanto a pagado el viajero 2");
+                    int viajero2= Integer.parseInt(s.nextLine());
+                }else {
+                    if(viajeros==3){
+                        System.out.println("Cuanto a pagado el viajero 1");
+                        int viajero1= Integer.parseInt(s.nextLine());
+                        System.out.println("Cuanto a pagado el viajero 2");
+                        int viajero2= Integer.parseInt(s.nextLine());
+                        System.out.println("Cuanto a pagado el viajero 3");
+                        int viajero3= Integer.parseInt(s.nextLine());
+                    }
+                }
+            }
+        }
     }
 }
